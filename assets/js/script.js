23d8ec0e2f09774b968fb8917eff2d1937b2aa43
KHALID IFQIRN <108679963+Khalid-Ifqirn@users.noreply.github.com>
2024-09-28 02:37:12 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+/*more details button*/
+const container = document.querySelector(".info-box");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+/*vibration*/
+function vibrate() {
+  navigator.vibrate(6);
+}
+
+// Zoom in up effect add in every social media button one by one
+const socialMediaLinks = document.querySelectorAll(".social-media div");
+
+// ON SCROLL REVEAL SMALL CARDS
+ScrollReveal().reveal(socialMediaLinks, {
+  duration: 1000,
+  opacity: 0,
+  distance: "30%",
+  origin: "bottom",
+  scale: 0.9,
+});
+
+// on load name effect 
+setTimeout(() => document.querySelector('.name').classList.add('loaded'), 500);
+
+const h6 = document.querySelector('h6');
+
+const typingAnimation = h6.animate([
+  { width: '0%' },
+  { width: '100%' }
+], {
+  duration: 2000,
+  easing: 'linear',
+  fill: 'forwards'
+});
+
+typingAnimation.onfinish = () => {
+    setTimeout(() => {
+      typingAnimation.play();
+    }, 3000);
+};
+
+
